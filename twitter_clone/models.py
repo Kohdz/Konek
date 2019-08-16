ac73c89149688918dc1ec91f7ec7287eb0288001
joe <jdlee6@buffalo.edu>
2019-08-16 19:18:14 -0400

    fixed directory tree

---
+++ twitter_clone/models.py
@@ -0,0 +1,9 @@
+from flask_login import UserMixin
+from twitter_clone import db
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    username = db.Column(db.String(30))
+    image = db.Column(db.String(100))
+    password = db.Column(db.String(50))
