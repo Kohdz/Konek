272800b4edb7e85668bc6b68441bd70b0505b554
kohdz <umair.stem@gmail.com>
2019-08-16 11:11:07 -0400

    basic html/routes created

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template(index.html)
+
+@app.route('/profile')
+def profile():
+    return render_template(profile.html)
+
+@app.route('/timeline')
+def timeline():
+    return render_template(timeline.html)
+
+
+@app.route('/register')
+def register():
+    return render_template(register.html)
