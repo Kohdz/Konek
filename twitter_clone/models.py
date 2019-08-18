93e6ad3103fa00320600620d74ee97e1cee285ae
joe <jdlee6@buffalo.edu>
2019-08-17 20:39:03 -0400

    register page error handling

--- twitter_clone/models.py
+++ twitter_clone/models.py
@@ -5,9 +5,10 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
     username = db.Column(db.String(30))
+    email = db.Column(db.String(120))
     image = db.Column(db.String(100))
     password = db.Column(db.String(50))
 
     # format of return string of "User.query.__() terminal command"
     def __repr__(self):
-        return f'username: {self.username} | name: {self.name}'
+        return f'username: {self.username} | name: {self.name} | email: {self.email}'
