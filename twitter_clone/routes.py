661519d2f8c6cfd62839a159f607c41ecad57bf4
kohdz <umair.stem@gmail.com>
2019-08-21 16:06:59 -0400

    minor changes implemented

--- twitter_clone/routes.py
+++ twitter_clone/routes.py
@@ -179,10 +179,13 @@ def timeline(username):
 
     current_time = datetime.now()
 
+    followed_by_count = user.followed_by.count()
+
     who_to_watch = User.query.filter(User.id != user.id).order_by(db.func.random()).limit(4).all()
 
     return render_template('timeline.html', title="Timeline", form=form, tweets=tweets,
-         current_time=current_time, current_user=user, total_tweets=total_tweets, who_to_watch=who_to_watch)
+         current_time=current_time, current_user=user, total_tweets=total_tweets, who_to_watch=who_to_watch,
+         followed_by_count=followed_by_count)
 
 
 @app.route('/post_tweet', methods=['POST'])
